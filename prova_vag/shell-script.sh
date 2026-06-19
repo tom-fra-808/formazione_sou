e08e10785ffeac6bdb9f118ae51381d49fc2a1c9
Tommaso <tommaso.franco@sourcesense.com>
2026-06-19 10:55:16 +0200

    Rimozione vecchi file eliminati

--- prova_vag/shell-script.sh
+++
@@ -1,8 +0,0 @@
-#install apache
-yum install --quiet -y httpd httpd-devel
-#copy conf files
-cp httpd.conf /etc/httpd/conf/httpd.conf
-cp httpd-vhosts /etc/httpd/conf/httpd-vhosts.conf
-#start apache and conf it to run at boot
-service httpd start
-chkconfig httpd on
